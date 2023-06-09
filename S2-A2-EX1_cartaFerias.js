ffafc298ffa5a2b4d0bc2bd34d5948d79f669c57
Caroline Sampaio <caroline_08022@hotmail.com>
2023-06-09 15:28:14 -0300

    Adiciona exercicio carta ferias

---
+++ S2-A2-EX1_cartaFerias.js
@@ -0,0 +1,23 @@
+var dataHoje = new Date().toLocaleDateString();
+
+var nomeFuncionario = prompt("Digite o nome do funcionário: ");
+var dataInicio = prompt("Digite a data de início das férias: ");
+var dataFim = prompt("Digite a data do fim das férias: ");
+
+var cartaFerias = `Caro(a) responsável,
+
+Gostaria de solicitar minhas férias conforme as informações abaixo: 
+
+Nome do funcionário: ${nomeFuncionario} 
+Data de Início: ${dataInicio}
+Data de Término: ${dataFim}
+
+Agradeço antecipamente pela atenção e aguardo a confirmação do meu pedido.
+
+Atenciosamente, 
+${nomeFuncionario}
+
+Data do pedido: ${dataHoje}
+`;
+
+console.log(cartaFerias);
