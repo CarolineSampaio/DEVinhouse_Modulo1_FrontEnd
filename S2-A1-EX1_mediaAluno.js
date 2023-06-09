abe9cd3630af34937e351a58a92e8402a38b1706
Caroline Sampaio <caroline_08022@hotmail.com>
2023-06-09 16:33:26 -0300

    Adiciona exercicio media aluno com funcao

--- S2-A1-EX1_mediaAluno.js
+++ S2-A1-EX1_mediaAluno.js
@@ -11,3 +11,24 @@ if (media >= 7) {
 } else {
   console.log("Aluno reprovado!");
 }
+
+// Com funcao
+
+function calcularMedia() {
+  var nota1 = Number(prompt("Digite a nota 1"));
+  var nota2 = Number(prompt("Digite a nota 2"));
+  var nota3 = Number(prompt("Digite a nota 3"));
+  var nota4 = Number(prompt("Digite a nota 4"));
+
+  var media = (nota1 + nota2 + nota3 + nota4) / 4;
+
+  if (media >= 7) {
+    console.log("aluno aprovado");
+  } else {
+    console.log("aluno em recuperacao");
+  }
+}
+
+calcularMedia();
+calcularMedia();
+calcularMedia();
