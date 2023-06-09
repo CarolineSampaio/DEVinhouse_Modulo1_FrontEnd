63414a7058447217e7fac06d83eb31730ee2c74c
Caroline Sampaio <caroline_08022@hotmail.com>
2023-06-09 16:36:46 -0300

    Adiciona exercicio calculadora FGTS com funcao

--- S2-A1-EX2_calculadoraFGTS.js
+++ S2-A1-EX2_calculadoraFGTS.js
@@ -4,3 +4,14 @@ var fgts = salarioBruto * 0.08;
 var anual = fgts * 12;
 console.log("O valor descontado mensalmente para FGTS é R$" + fgts);
 console.log("Ao final de um ano você acumulará, em FGTS, R$" + anual);
+
+// Com função
+function calculofgts() {
+  var salarioBruto = Number(prompt("Digite seu salário bruto"));
+  var fgts = salarioBruto * 0.08;
+  var anual = fgts * 12;
+  console.log("O valor descontado mensalmente para FGTS é R$" + fgts);
+  console.log("Ao final de um ano você acumulará, em FGTS, R$" + anual);
+}
+
+calculofgts();
