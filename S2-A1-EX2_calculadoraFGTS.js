f0fd6ca374cf108621d6d3e1a515a945704ca1f6
Caroline Sampaio <caroline_08022@hotmail.com>
2023-06-09 14:32:20 -0300

    Adiciona exercicio 2 calcudora FGTS

---
+++ S2-A1-EX2_calculadoraFGTS.js
@@ -0,0 +1,6 @@
+//Construa um algoritmo que calcule o FGTS de um funcionário com base no salário bruto recebido. Exiba o resultado no terminal.
+var salarioBruto = Number(prompt("Digite seu salário bruto"));
+var fgts = salarioBruto * 0.08;
+var anual = fgts * 12;
+console.log("O valor descontado mensalmente para FGTS é R$" + fgts);
+console.log("Ao final de um ano você acumulará, em FGTS, R$" + anual);
