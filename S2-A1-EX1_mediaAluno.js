862a45a6b87a13805c3ef653533d54b08e5adf67
Caroline Sampaio <caroline_08022@hotmail.com>
2023-06-09 14:41:43 -0300

    Adiciona exercicio 1 media aluno com condicionais

--- S2-A1-EX1_mediaAluno.js
+++ S2-A1-EX1_mediaAluno.js
@@ -3,5 +3,11 @@ var nota1 = Number(prompt("Digite a nota 1"));
 var nota2 = Number(prompt("Digite a nota 2"));
 var nota3 = Number(prompt("Digite a nota 3"));
 var nota4 = Number(prompt("Digite a nova 4"));
+
 var media = (nota1 + nota2 + nota3 + nota4) / 4;
-console.log(media);
+
+if (media >= 7) {
+  console.log("Aluno aprovado!");
+} else {
+  console.log("Aluno reprovado!");
+}
