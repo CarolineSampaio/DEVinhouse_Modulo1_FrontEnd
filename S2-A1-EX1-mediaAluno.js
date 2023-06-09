1414cc2152cdb467c11425645e20ea7446b6a77c
Caroline <113152914+CarolineSampaio@users.noreply.github.com>
2023-06-09 14:25:18 -0300

    Deletando arquivo duplicado

--- S2-A1-EX1-mediaAluno.js
+++
@@ -1,7 +0,0 @@
-//Construa um algoritmo que calcule a média aritmética de um aluno (N1+ N2+ N3+ N4).
-var nota1 = Number(prompt("Digite a nota 1"));
-var nota2 = Number(prompt("Digite a nota 2"));
-var nota3 = Number(prompt("Digite a nota 3"));
-var nota4 = Number(prompt("Digite a nova 4"));
-var media = (nota1 + nota2 + nota3 + nota4) / 4;
-console.log(media);
