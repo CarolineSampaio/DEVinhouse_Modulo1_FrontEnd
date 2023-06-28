ef52088936868bd1e385a26ef09b5878c8a6a932
Caroline Sampaio <caroline_08022@hotmail.com>
2023-06-28 12:41:14 -0300

    atualiza exemplo aula 2 tela login instagram

--- Semana 4/S4-A2-EX1_script.js
+++ Semana 4/S4-A2-EX1_script.js
@@ -2,7 +2,29 @@ function clicaNoBotao() {
   var login = document.getElementById("campoLogin").value;
   var senha = document.getElementById("campoSenha").value;
 
+  document.getElementById("campoLogin").classList.remove("input-error");
+  document.getElementById("campoSenha").classList.remove("input-error");
+
   if (login === "") {
-    alert("Login inválido. Insira um telefone, nome de usuário ou email.");
+    /* adiciona um estilo inline */
+    // document.getElementById('campo-email').style = "border-color: red"
+
+    /* adiciona um estilo direto no objeto */
+    // document.getElementById('campo-email').style.backgroundColor = 'red'
+
+    /* adiciona uma nova classe ao objeto e devemos configurar css */
+    document.getElementById("campoLogin").classList.add("input-error");
+    /* foca o cursor no elemento vazio */
+    document.getElementById("campoLogin").focus();
+  } else if (senha === "") {
+    document.getElementById("campoSenha").classList.add("input-error");
+    document.getElementById("campoLogin").focus();
+  } else {
+    document.getElementById("loginButton").disabled = true;
+    document.getElementById("loginButton").style.opacity = 0.5;
+    document.getElementById("loginButton").innerText = "Logando...";
+
+    // redireciona para outra página
+    window.location.href = "https://www.google.com/";
   }
 }
