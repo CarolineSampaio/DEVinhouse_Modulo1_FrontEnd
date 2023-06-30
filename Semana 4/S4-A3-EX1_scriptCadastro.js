b5d727544891049cc1113c25df3f986fed956948
Caroline Sampaio <caroline_08022@hotmail.com>
2023-06-30 17:21:11 -0300

    Atualiza tela de cadastro verificacoes dos campos

--- Semana 4/S4-A3-EX1_scriptCadastro.js
+++ Semana 4/S4-A3-EX1_scriptCadastro.js
@@ -1,3 +1,6 @@
+var regexEmail = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+var regexTelefone = /^\(?\d{2}\)?\s?\d{5}-?\d{4}$/;
+
 function fazerCadastro() {
   var email = document.getElementById("campoEmail").value;
   var telefone = document.getElementById("campoTelefone").value;
@@ -5,12 +8,48 @@ function fazerCadastro() {
   var nomeUsuario = document.getElementById("campoNomeUsuario").value;
   var senha = document.getElementById("campoSenha").value;
 
+  document.getElementById("campoEmail").classList.remove("inputError");
+  document.getElementById("errorEmail").hidden = true;
+
+  document.getElementById("campoTelefone").classList.remove("inputError");
+  document.getElementById("errorTelefone").hidden = true;
+
+  document.getElementById("campoNomeCompleto").classList.remove("inputError");
+  document.getElementById("errorNome").hidden = true;
+
+  document.getElementById("campoNomeUsuario").classList.remove("inputError");
+  document.getElementById("errorUsuario").hidden = true;
+
+  document.getElementById("campoSenha").classList.remove("inputError");
+  document.getElementById("errorSenha").hidden = true;
+
+  if (regexEmail.test(email) === false) {
+    document.getElementById("campoEmail").classList.add("inputError");
+    document.getElementById("errorEmail").hidden = false;
+    document.getElementById("errorEmail").innerText = "Digite um email válido";
+  }
+
+  if (regexTelefone.test(telefone) === false) {
+    document.getElementById("campoTelefone").classList.add("inputError");
+    document.getElementById("errorTelefone").hidden = false;
+    document.getElementById("errorTelefone").innerText =
+      "Digite um telefone válido";
+  }
+
   if (nomeCompleto === "") {
     document.getElementById("campoNomeCompleto").classList.add("inputError");
-    document.getElementById("campoNomeCompleto").focus();
     document.getElementById("errorNome").hidden = false;
     document.getElementById("errorNome").innerText = "O nome é obrigatório";
-  } else if (senha.length < 9) {
+  }
+
+  if (nomeUsuario === "") {
+    document.getElementById("campoNomeUsuario").classList.add("inputError");
+    document.getElementById("errorUsuario").hidden = false;
+    document.getElementById("errorUsuario").innerText =
+      "O usuário é obrigatório";
+  }
+
+  if (senha.length < 9) {
     document.getElementById("campoSenha").classList.add("inputError");
     document.getElementById("errorSenha").hidden = false;
     document.getElementById("errorSenha").innerText =
