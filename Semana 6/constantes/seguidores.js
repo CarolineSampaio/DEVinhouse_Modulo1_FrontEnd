963a54f1d2a12f80e6f086e243c1d26d300daa61
Caroline Sampaio <caroline_08022@hotmail.com>
2023-07-06 16:46:02 -0300

    atualiza exemplo aula 3 tela de seguidores

---
+++ Semana 6/constantes/seguidores.js
@@ -0,0 +1,74 @@
+export const seguidores = [
+  {
+    url: "https://img.freepik.com/fotos-gratis/pessoa-de-origem-indiana-se-divertindo_23-2150285283.jpg?w=740&t=st=1688598633~exp=1688599233~hmac=50bfd56c918c046b6a624c9abcff17dd937c453d73afd8d00b98a21ff177e72a",
+    nome: "João Silva",
+    quantidadeSeguidores: 1000,
+    quantidadePublicacoes: 50,
+  },
+  {
+    url: "https://img.freepik.com/fotos-gratis/pessoa-de-origem-indiana-se-divertindo_23-2150285283.jpg?w=740&t=st=1688598633~exp=1688599233~hmac=50bfd56c918c046b6a624c9abcff17dd937c453d73afd8d00b98a21ff177e72a",
+    nome: "Maria Santos",
+    quantidadeSeguidores: 500,
+    quantidadePublicacoes: 120,
+  },
+  {
+    url: "https://img.freepik.com/fotos-gratis/pessoa-de-origem-indiana-se-divertindo_23-2150285283.jpg?w=740&t=st=1688598633~exp=1688599233~hmac=50bfd56c918c046b6a624c9abcff17dd937c453d73afd8d00b98a21ff177e72a",
+    nome: "Pedro Oliveira",
+    quantidadeSeguidores: 200,
+    quantidadePublicacoes: 75,
+  },
+  {
+    url: "https://img.freepik.com/fotos-gratis/pessoa-de-origem-indiana-se-divertindo_23-2150285283.jpg?w=740&t=st=1688598633~exp=1688599233~hmac=50bfd56c918c046b6a624c9abcff17dd937c453d73afd8d00b98a21ff177e72a",
+    nome: "Ana Souza",
+    quantidadeSeguidores: 800,
+    quantidadePublicacoes: 100,
+  },
+  {
+    url: "https://img.freepik.com/fotos-gratis/pessoa-de-origem-indiana-se-divertindo_23-2150285283.jpg?w=740&t=st=1688598633~exp=1688599233~hmac=50bfd56c918c046b6a624c9abcff17dd937c453d73afd8d00b98a21ff177e72a",
+    nome: "Lucas Pereira",
+    quantidadeSeguidores: 350,
+    quantidadePublicacoes: 200,
+  },
+  {
+    url: "https://img.freepik.com/fotos-gratis/pessoa-de-origem-indiana-se-divertindo_23-2150285283.jpg?w=740&t=st=1688598633~exp=1688599233~hmac=50bfd56c918c046b6a624c9abcff17dd937c453d73afd8d00b98a21ff177e72a",
+    nome: "Camila Alves",
+    quantidadeSeguidores: 1500,
+    quantidadePublicacoes: 90,
+  },
+  {
+    url: "https://img.freepik.com/fotos-gratis/pessoa-de-origem-indiana-se-divertindo_23-2150285283.jpg?w=740&t=st=1688598633~exp=1688599233~hmac=50bfd56c918c046b6a624c9abcff17dd937c453d73afd8d00b98a21ff177e72a",
+    nome: "João Silva",
+    quantidadeSeguidores: 1000,
+    quantidadePublicacoes: 50,
+  },
+  {
+    url: "https://img.freepik.com/fotos-gratis/pessoa-de-origem-indiana-se-divertindo_23-2150285283.jpg?w=740&t=st=1688598633~exp=1688599233~hmac=50bfd56c918c046b6a624c9abcff17dd937c453d73afd8d00b98a21ff177e72a",
+    nome: "Maria Santos",
+    quantidadeSeguidores: 500,
+    quantidadePublicacoes: 120,
+  },
+  {
+    url: "https://img.freepik.com/fotos-gratis/pessoa-de-origem-indiana-se-divertindo_23-2150285283.jpg?w=740&t=st=1688598633~exp=1688599233~hmac=50bfd56c918c046b6a624c9abcff17dd937c453d73afd8d00b98a21ff177e72a",
+    nome: "Pedro Oliveira",
+    quantidadeSeguidores: 200,
+    quantidadePublicacoes: 75,
+  },
+  {
+    url: "https://img.freepik.com/fotos-gratis/pessoa-de-origem-indiana-se-divertindo_23-2150285283.jpg?w=740&t=st=1688598633~exp=1688599233~hmac=50bfd56c918c046b6a624c9abcff17dd937c453d73afd8d00b98a21ff177e72a",
+    nome: "Ana Souza",
+    quantidadeSeguidores: 800,
+    quantidadePublicacoes: 100,
+  },
+  {
+    url: "https://img.freepik.com/fotos-gratis/pessoa-de-origem-indiana-se-divertindo_23-2150285283.jpg?w=740&t=st=1688598633~exp=1688599233~hmac=50bfd56c918c046b6a624c9abcff17dd937c453d73afd8d00b98a21ff177e72a",
+    nome: "Lucas Pereira",
+    quantidadeSeguidores: 350,
+    quantidadePublicacoes: 200,
+  },
+  {
+    url: "https://img.freepik.com/fotos-gratis/pessoa-de-origem-indiana-se-divertindo_23-2150285283.jpg?w=740&t=st=1688598633~exp=1688599233~hmac=50bfd56c918c046b6a624c9abcff17dd937c453d73afd8d00b98a21ff177e72a",
+    nome: "Camila Alves",
+    quantidadeSeguidores: 1500,
+    quantidadePublicacoes: 90,
+  },
+];
