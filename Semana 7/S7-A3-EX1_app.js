5c7b50e4b66fce48f67bd5e7e7d9ace1cef52bfb
Caroline Sampaio <caroline_08022@hotmail.com>
2023-07-13 15:51:28 -0300

    atualiza aula 3 criação de item tarefa checkbox

--- Semana 7/S7-A3-EX1_app.js
+++ Semana 7/S7-A3-EX1_app.js
@@ -1,15 +1,27 @@
 import Item from "./S7-A3-EX1_item.js";
+import itemTarefa from "./S7-A3-EX1_itemTarefa.js";
 import { dadosLista } from "./S7-A3-EX1_dados.js";
 
 console.log("m1_s07_a3");
 
-const elemLista = document.getElementById("lista-itens");
+const elemFormulario = document.querySelector("#formulario");
+const elemLista = document.querySelector("#lista-itens");
+const campoTitulo = document.getElementById("titulo-item");
+const campoTipo = document.getElementById("tipo-item");
 let listaControle = [];
 
-function criaInstanciasItens(lista) {
-  return lista.map((item) => {
-    return new Item(item);
-  });
+function criaInstanciaItem(dadosItem) {
+  switch (dadosItem.tipo) {
+    case "tarefa":
+      return new itemTarefa(dadosItem);
+
+    default:
+      return new Item(dadosItem);
+  }
+}
+
+function populaListaControle(listaCrua) {
+  listaControle = listaCrua.map(criaInstanciaItem);
 }
 
 function atualizaTela() {
@@ -21,9 +33,39 @@ function atualizaTela() {
   });
 }
 
-// função anônima de uso único
-(function () {
-  // inicialização da nossa aplicação
-  listaControle = criaInstanciasItens(dadosLista);
+function adicionaItem(item) {
+  const novaInstanciaItem = criaInstanciaItem(item);
+  listaControle.push(novaInstanciaItem);
+  console.log(listaControle);
+}
+
+function processaSubmit(evento) {
+  // prevenir a execução do comportamento padrão de submit
+  evento.preventDefault();
+
+  // agrupando valores do formulario
+  const novoItem = {
+    id: Date.now(), // id unico milissegundos
+    titulo: campoTitulo.value,
+    tipo: campoTipo.value,
+  };
+
+  if (novoItem.tipo === "tarefa") {
+    novoItem.concluida = false;
+  }
+
+  adicionaItem(novoItem);
+  elemFormulario.reset();
   atualizaTela();
+}
+
+// função anônima de uso único
+// inicialização da nossa aplicação
+(function () {
+  // popula lista de controle
+  populaListaControle(dadosLista);
+  // atualiza a tela
+  atualizaTela();
+  // adiciona escutadores de eventos
+  elemFormulario.addEventListener("submit", processaSubmit);
 })();
