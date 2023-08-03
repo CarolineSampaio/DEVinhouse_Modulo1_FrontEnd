c45f9e8d2e3cf6ec2ef5326bddb02dbbc0f1e312
Caroline Sampaio <caroline_08022@hotmail.com>
2023-08-03 20:26:21 -0300

    add exemplo inicial aula 2, operador ternario vue

---
+++ Semanas_vue/semana_9/aula_2/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h1>Olá, {{ nome }}!</h1>
+    <p>Numero: {{ numero % 2 === 0 ? "PAR" : "ÍMPAR" }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      nome: "João",
+      numero: 10,
+    };
+  },
+};
+</script>
+
+<style></style>
