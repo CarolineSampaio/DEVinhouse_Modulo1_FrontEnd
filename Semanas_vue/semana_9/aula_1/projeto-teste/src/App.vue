fbf58c47e8f5967c916082669f836acd7fb47321
Caroline Sampaio <caroline_08022@hotmail.com>
2023-08-03 18:27:09 -0300

    add estrutura inicial arquivo vue, exemplo aula 1

---
+++ Semanas_vue/semana_9/aula_1/projeto-teste/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <!-- SINGLE FILE COMPONENT -->
+  <div class="container">
+    <h1>Hello, world!</h1>
+    <h2>{{ nome }}</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      nome: "Bruno costa",
+      usuario: {
+        idade: 24,
+        email: "email@email.com",
+      },
+    };
+  },
+};
+</script>
+
+<style>
+.container {
+  width: 100vw;
+  height: 100vh;
+  background-color: rgb(255, 167, 167);
+}
+</style>
