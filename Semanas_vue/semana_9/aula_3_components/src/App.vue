622967f765c52470990f65eec12fb40ffed6dfc1
Caroline Sampaio <caroline_08022@hotmail.com>
2023-08-04 18:16:43 -0300

    add exemplo inicial components aula 3

---
+++ Semanas_vue/semana_9/aula_3_components/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h1>Hello, world!</h1>
+    <Card>
+      <p>Conteúdo do slot</p>
+    </Card>
+  </div>
+</template>
+
+<script>
+import Card from "./components/Card.vue";
+
+export default {
+  name: "App",
+  components: {
+    Card,
+  },
+};
+</script>
+
+<style scoped></style>
---
+++ Semanas_vue/semana_9/aula_3_components/src/components/Card.vue
@@ -0,0 +1,14 @@
+<template>
+  <h1>Card teste</h1>
+
+  <!-- slot é um espaço reservado para receber conteúdo -->
+  <slot></slot>
+</template>
+
+<script></script>
+
+<style scoped>
+h1 {
+  color: red;
+}
+</style>
