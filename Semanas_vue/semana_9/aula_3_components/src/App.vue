fed7338da6e9095de3daccc1a3ee6d9348f47a0a
Caroline Sampaio <caroline_08022@hotmail.com>
2023-08-04 18:29:23 -0300

    atualiza componente card com slots

--- Semanas_vue/semana_9/aula_3_components/src/App.vue
+++ Semanas_vue/semana_9/aula_3_components/src/App.vue
@@ -1,8 +1,18 @@
 <template>
   <div>
-    <h1>Hello, world!</h1>
     <Card>
-      <p>Conteúdo do slot</p>
+      <template v-slot:header>
+        <h1>Titulo do card</h1>
+      </template>
+      <p>
+        Conteúdo do slot default, sem nome. Lorem ipsum dolor sit amet. Sed
+        porro dolorem ut aliquam tenetur qui officiis ratione id omnis
+        voluptates quo voluptatum expedita! Et aliquam similique aut esse sint
+        qui rerum labore cum officiis culpa sit odio omnis.
+      </p>
+      <template #footer>
+        <h6>Created by Caroline Sampaio</h6>
+      </template>
     </Card>
   </div>
 </template>
--- Semanas_vue/semana_9/aula_3_components/src/components/Card.vue
+++ Semanas_vue/semana_9/aula_3_components/src/components/Card.vue
@@ -1,14 +1,14 @@
 <template>
-  <h1>Card teste</h1>
-
-  <!-- slot é um espaço reservado para receber conteúdo -->
+  <header>
+    <slot name="header"></slot>
+  </header>
   <slot></slot>
+
+  <footer>
+    <slot name="footer"></slot>
+  </footer>
 </template>
 
 <script></script>
 
-<style scoped>
-h1 {
-  color: red;
-}
-</style>
+<style scoped></style>
