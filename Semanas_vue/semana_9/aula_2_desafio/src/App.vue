e2c2561392ad6233ab5ad6dadd55096925c17da9
Caroline Sampaio <caroline_08022@hotmail.com>
2023-08-03 21:54:26 -0300

    add exercicio verifica par ou impar desafio aula 2

---
+++ Semanas_vue/semana_9/aula_2_desafio/src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <h1>PAR / ÍMPAR</h1>
+    <div>
+      <label>Digite um número: </label>
+      <input type="number" v-model="number" />
+      <button @click="verificaNumero()">Verificar</button>
+      <span>{{ resultado }}</span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      number: 0,
+      resultado: "",
+    };
+  },
+  methods: {
+    verificaNumero() {
+      if (!!this.number == false) {
+        alert("Digite um número!");
+        return;
+      }
+
+      if (this.number % 2 == 0) {
+        this.resultado = "O número " + this.number + " é par.";
+      } else {
+        this.resultado = "O número " + this.number + " é ímpar.";
+      }
+    },
+  },
+};
+</script>
+
+<style></style>
