d983832e190abaf20d4f28c6579645de19a763e8
Caroline Sampaio <caroline_08022@hotmail.com>
2023-08-04 20:18:14 -0300

    add exercicio renderização listas com vue aula 3

---
+++ Semanas_vue/semana_9/aula_3_lista_cursos/src/App.vue
@@ -0,0 +1,46 @@
+<template>
+  <div>
+    <h1>Lista de Cursos</h1>
+    <!-- O v-bind ou : é uma diretiva que nos permite passar valores Javascript dentro das props  -->
+    <CursoItem
+      v-for="(curso, index) in listaCursos"
+      :key="index"
+      :titulo="curso.titulo"
+      :duracao="curso.duracao"
+      :vagas="curso.vagas"
+    />
+  </div>
+</template>
+
+<script>
+import CursoItem from "./components/CursoItem.vue";
+export default {
+  components: {
+    CursoItem,
+  },
+
+  data() {
+    return {
+      listaCursos: [
+        {
+          titulo: "HTML e CSS",
+          duracao: 20,
+          vagas: 40,
+        },
+        {
+          titulo: "Javascript",
+          duracao: 20,
+          vagas: 40,
+        },
+        {
+          titulo: "PHP",
+          duracao: 20,
+          vagas: 40,
+        },
+      ],
+    };
+  },
+};
+</script>
+
+<style scoped></style>
---
+++ Semanas_vue/semana_9/aula_3_lista_cursos/src/components/CursoItem.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <h1>Título: {{ titulo }}</h1>
+    <span>Duração: {{ duracao }} horas</span> <br />
+    <span>Vagas: {{ vagas }} vagas</span>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["titulo", "duracao", "vagas"],
+};
+</script>
