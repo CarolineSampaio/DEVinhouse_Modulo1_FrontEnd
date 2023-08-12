3279d6ad1cf9c910860db96e0b00e60b5c0658f8
Caroline Sampaio <caroline_08022@hotmail.com>
2023-08-12 16:36:58 -0300

    atualiza estilo formulario cadastro

--- Semanas_vue/semana_10/clone_instagram/src/views/SignUp/SignUp.vue
+++ Semanas_vue/semana_10/clone_instagram/src/views/SignUp/SignUp.vue
@@ -1,48 +1,76 @@
 <template>
-  <form class="formLogin">
+  <form @submit.prevent="handleCreateAccount" class="formLogin">
     <img src="../../assets/logo.png" alt="Logo" />
-    <h3>Cadastro influenciador</h3>
+    <h3>Cadastro Influenciador</h3>
 
-    <label for="nome">Nome Completo</label>
-    <input v-model="nome" type="text" id="nome" name="nome" />
+    <div>
+      <label for="nome">Nome Completo</label>
+      <input v-model="nome" type="text" id="nome" name="nome" class="formInput" />
+    </div>
 
-    <label for="email">Email</label>
-    <input v-model="email" type="text" id="email" name="email" />
+    <div>
+      <label for="email">Email</label>
+      <input v-model="email" type="text" id="email" name="email" class="formInput" />
+    </div>
 
-    <label for="telefone">Telefone</label>
-    <input v-model="telefone" type="text" id="telefone" name="telefone" />
+    <div>
+      <label for="telefone">Telefone</label>
+      <input v-model="telefone" type="text" id="telefone" name="telefone" class="formInput" />
+    </div>
 
-    <label for="password">Senha</label>
-    <input v-model="password" type="password" id="password" name="password" />
+    <div>
+      <label for="password">Senha</label>
+      <input v-model="password" type="password" id="password" name="password" class="formInput" />
+    </div>
 
-    <label for="confirmPassword">Confirmar Senha</label>
-    <input v-model="confirmPassword" type="password" id="confirmPassword" name="confirmPassword" />
+    <div>
+      <label for="confirmPassword">Confirmar Senha</label>
+      <input
+        v-model="confirmPassword"
+        type="password"
+        id="confirmPassword"
+        name="confirmPassword"
+        class="formInput"
+      />
+    </div>
 
-    <select id="sponsor" v-model="sponsor">
-      <option value="" disabled selected>Selecione um patrocinador</option>
-      <option value="Mercado Livre">Mercado Livre</option>
-      <option value="Magazine Luiza">Magazine Luiza</option>
-      <option value="Amazon">Amazon</option>
-      <option value="Americanas">Americanas</option>
-      <option value="Submarino">Submarino</option>
-    </select>
+    <div>
+      <label for="sponsor">Patrocinador</label>
+      <select id="sponsor" v-model="sponsor">
+        <option value="" disabled selected>Selecione um patrocinador</option>
+        <option value="Mercado Livre">Mercado Livre</option>
+        <option value="Magazine Luiza">Magazine Luiza</option>
+        <option value="Amazon">Amazon</option>
+        <option value="Americanas">Americanas</option>
+        <option value="Submarino">Submarino</option>
+      </select>
+    </div>
 
-    <label for="bio">Sua biografia</label>
-    <textarea v-model="bio" id="bio" rows="12"></textarea>
+    <div>
+      <label for="bio">Sua biografia</label>
+      <textarea class="textArea" v-model="bio" id="bio" rows="12"></textarea>
+    </div>
+
+    <div>
+      <p>Selecione uma categoria:</p>
+      <div>
+        <input v-model="planType" id="bronze" type="radio" value="1" />
+        <label for="bronze">Bronze</label>
+      </div>
+      <div>
+        <input v-model="planType" id="prata" type="radio" value="2" />
+        <label for="prata">Prata</label>
+      </div>
+      <div>
+        <input v-model="planType" id="premium" type="radio" value="3" />
+        <label for="premium">Premium</label>
+      </div>
+    </div>
 
     <label id="confirmTerms">
       <input v-model="confirmTerms" type="checkbox" id="termos" name="termos" />
       Aceitar Termos de Uso
     </label>
-
-    <input v-model="planType" id="bronze" type="radio" value="1" />
-    <label for="bronze">Bronze</label>
-
-    <input v-model="planType" id="prata" type="radio" value="2" />
-    <label for="prata">Prata</label>
-
-    <input v-model="planType" id="premium" type="radio" value="3" />
-    <label for="premium">Premium</label>
 
     <button type="submit">Criar conta</button>
   </form>
@@ -62,6 +90,11 @@ export default {
       confirmTerms: false,
       planType: '2'
     }
+  },
+  methods: {
+    handleCreateAccount() {
+      alert('Conta criada com sucesso!')
+    }
   }
 }
 </script>
@@ -76,29 +109,36 @@ export default {
 
   display: flex;
   flex-direction: column;
-  gap: 20px;
+  gap: 12px;
   align-items: center;
   padding: 10px;
 }
 
-.inputArea {
-  width: 80%;
-
+.formLogin > div {
   display: flex;
   flex-direction: column;
-}
-
-.inputArea input {
-  width: 100%;
-}
-
-input {
-  height: 40px;
   width: 80%;
+}
+
+.formLogin > div > div {
+  display: flex;
+  align-items: center;
+  gap: 5px;
+}
+
+.formInput,
+.textArea,
+select {
+  height: 40px;
+  width: 100%;
   border-radius: 4px;
   border: 1px solid #dddddd;
   outline: none;
   background-color: rgb(250, 250, 250);
+}
+
+.textArea {
+  height: 150px;
 }
 
 button {
