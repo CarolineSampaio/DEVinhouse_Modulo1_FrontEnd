7fd5bff21f2f2ea92e31071ee5b2942f318e229c
Caroline Sampaio <caroline_08022@hotmail.com>
2023-08-14 21:43:50 -0300

    feat: adiciona elementos e estilo na pagina

--- Semanas_vue/semana_10/exercicio_semana10/banco_talentos/src/views/About/About.vue
+++ Semanas_vue/semana_10/exercicio_semana10/banco_talentos/src/views/About/About.vue
@@ -1,2 +1,47 @@
 <!-- eslint-disable vue/multi-word-component-names -->
-<template><div>Pagina Sobre</div></template>
+<template>
+  <main>
+    <h1 class="title">Sobre</h1>
+
+    <p>
+      Lorem ipsum dolor sit amet. Eos magni autem quo tempore quia et ipsa asperiores At delectus
+      nesciunt aut recusandae illum aut voluptas sunt et nobis facere. Aut minus quae et similique
+      repudiandae quo laboriosam sapiente ex aspernatur enim a reiciendis sapiente ab temporibus
+      quas aut quas autem.
+    </p>
+
+    <p>
+      Ad eveniet enim vel velit aspernatur in dolor animi ut assumenda fugiat et debitis dolor nam
+      veniam excepturi. Aut dolores autem et eaque porro et odio assumenda eum facilis adipisci. 0
+      Rem perspiciatis suscipit qui alias rerum ut facilis blanditiis sit dolorum atque aut iusto
+      temporibus!
+    </p>
+
+    <p>
+      Ut repudiandae aliquam et dolore voluptas ad provident enim ut obcaecati quae et velit natus
+      ea eligendi tenetur qui ullam saepe. Aut animi omnis aut voluptatem perspiciatis ut fugiat
+      tempore. Ut ullam consequatur aut adipisci mollitia ea esse cumque.
+    </p>
+  </main>
+</template>
+
+<style scoped>
+.title {
+  color: #2b2b2b;
+  font-size: 2rem;
+  font-weight: 700;
+  margin: 0;
+  padding: 0;
+  margin-bottom: 3rem;
+}
+
+p {
+  margin: 0;
+  padding: 0;
+  margin-bottom: 1rem;
+  line-height: 1.5rem;
+
+  color: #727272;
+  font-weight: 500;
+}
+</style>
--- Semanas_vue/semana_10/exercicio_semana10/banco_talentos/src/views/Talent/NewTalent.vue
+++ Semanas_vue/semana_10/exercicio_semana10/banco_talentos/src/views/Talent/NewTalent.vue
@@ -1,2 +1,22 @@
 <!-- eslint-disable vue/multi-word-component-names -->
-<template><div>cadastro de talento</div></template>
+<template>
+  <main>
+    <h1 class="title">Cadastro de talento</h1>
+
+    <formulario>
+      <label for="nome">Nome completo</label>
+      <input type="text" id="nome" name="nome" placeholder="Nome" />
+    </formulario>
+  </main>
+</template>
+
+<style scoped>
+.title {
+  color: #2b2b2b;
+  font-size: 2rem;
+  font-weight: 700;
+  margin: 0;
+  padding: 0;
+  margin-bottom: 2rem;
+}
+</style>
