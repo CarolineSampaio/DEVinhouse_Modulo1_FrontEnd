b832eaea42610a3121e8c424c715dc59e70fbb19
Caroline Sampaio <caroline_08022@hotmail.com>
2023-08-14 21:24:45 -0300

    feat: add menu inicial padrao para paginas

--- Semanas_vue/semana_10/exercicio_semana10/banco_talentos/src/App.vue
+++ Semanas_vue/semana_10/exercicio_semana10/banco_talentos/src/App.vue
@@ -1,4 +1,21 @@
 <template>
+  <header>
+    <ul>
+      <li>
+        <router-link to="/">Home</router-link>
+        <img width="35" src="./assets/icons/homeIcon.svg" alt="icone home" />
+      </li>
+
+      <li>
+        <router-link to="/talento/novo">Talentos</router-link>
+        <img width="35" src="./assets/icons/talentIcon.svg" alt="opened-folder" />
+      </li>
+      <li>
+        <router-link to="/about">About</router-link>
+        <img width="35" src="./assets/icons/sobreIcon.svg" alt="map-marker--v1" />
+      </li>
+    </ul>
+  </header>
   <main>
     <router-view></router-view>
   </main>
@@ -6,4 +23,28 @@
 
 <script></script>
 
-<style></style>
+<style scoped>
+ul {
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  list-style: none;
+  margin: 30px 0px 60px;
+}
+
+li {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 40px;
+
+  border: 1px solid #d5dee5;
+  padding: 5px 20px;
+}
+
+a {
+  text-decoration: none;
+  color: #000;
+}
+</style>
