da4adf568fa4d8dde0cf11639a9d8ed00b3c9519
Caroline Sampaio <caroline_08022@hotmail.com>
2023-08-14 21:27:29 -0300

    style: atualiza estilo do titulo da pagina

--- Semanas_vue/semana_10/exercicio_semana10/banco_talentos/src/views/Home/Home.vue
+++ Semanas_vue/semana_10/exercicio_semana10/banco_talentos/src/views/Home/Home.vue
@@ -1,2 +1,18 @@
 <!-- eslint-disable vue/multi-word-component-names -->
-<template><div>Pagina Home</div></template>
+<template>
+  <main>
+    <h1 class="title">Banco de talentos</h1>
+  </main>
+</template>
+
+<style scoped>
+.title {
+  color: #2b2b2b;
+  font-size: 2rem;
+  font-weight: 700;
+  text-align: center;
+  margin: 0;
+  padding: 0;
+  margin-bottom: 1rem;
+}
+</style>
