12c2023ae6fb2ee86c9c97cecba28cc145ddb346
Caroline Sampaio <caroline_08022@hotmail.com>
2023-08-21 14:52:13 -0300

    feat: add validação cadastro com yup

--- Semanas_vue/semana_10/exercicio_semana10/banco_talentos/src/views/Talent/NewTalent.vue
+++ Semanas_vue/semana_10/exercicio_semana10/banco_talentos/src/views/Talent/NewTalent.vue
@@ -3,47 +3,73 @@
   <main>
     <h1 class="title">Cadastro de talento</h1>
 
-    <formulario class="formLogin">
+    <form @submit.prevent="handleSubmit" class="formNew">
       <div class="formElement">
         <div>
           <label for="name">Nome completo</label>
-          <input id="name" v-model="name" class="formInput" />
+          <input
+            id="name"
+            v-model="name"
+            class="formInput"
+            :class="{ inputError: this.errors.nome }"
+          />
+          <span class="textError">{{ errors.nome }}</span>
         </div>
         <div>
           <label for="email">Email</label>
-          <input id="email" v-model="email" class="formInput" />
+          <input
+            id="email"
+            v-model="email"
+            class="formInput"
+            :class="{ inputError: this.errors.email }"
+          />
+          <span class="textError">{{ errors.email }}</span>
         </div>
       </div>
 
       <div class="formElement">
         <div>
           <label for="birthDate">Data de nascimento</label>
-          <input id="birthDate" v-model="birthDate" class="formInput" />
+          <input
+            id="birthDate"
+            v-model="birthDate"
+            class="formInput"
+            :class="{ inputError: this.errors.birthDate }"
+          />
+          <span class="textError">{{ errors.birthDate }}</span>
         </div>
         <div>
           <label for="phone">Whatsapp</label>
-          <input id="phone" v-model="phone" class="formInput" />
+          <input
+            id="phone"
+            v-model="phone"
+            class="formInput"
+            :class="{ inputError: this.errors.phone }"
+          />
+          <span class="textError">{{ errors.phone }}</span>
         </div>
       </div>
 
       <div class="formSelect">
         <label for="area">Área de interesse</label>
-        <select v-model="area">
+        <select v-model="area" :class="{ inputError: this.errors.area }">
           <option value="" disabled selected></option>
           <option value="frontend">FrontEnd</option>
           <option value="backend">BackEnd</option>
           <option value="fullstack">FullStack</option>
         </select>
+        <span class="textError">{{ errors.area }}</span>
       </div>
 
       <div class="formSelect">
         <label for="nivelProfissional">Nível profissional</label>
-        <select v-model="nivel">
+        <select v-model="nivel" :class="{ inputError: this.errors.nivel }">
           <option value="" disabled selected></option>
           <option value="junior">Junior</option>
           <option value="pleno">Pleno</option>
           <option value="senior">Senior</option>
         </select>
+        <span class="textError">{{ errors.nivel }}</span>
       </div>
 
       <div class="formSelect">
@@ -74,21 +100,26 @@
           <label><input type="checkbox" v-model="skills" value="Vue" />Vue</label>
           <label><input type="checkbox" v-model="skills" value="Angular" />Angular</label>
         </div>
+        <span class="textError">{{ errors.skills }}</span>
       </div>
 
       <div>
         <textarea
           v-model="presentation"
           placeholder="Digite aqui sua carta de apresentação"
+          :class="{ inputError: this.errors.presentation }"
         ></textarea>
+        <span class="textError">{{ errors.presentation }}</span>
       </div>
 
       <button type="submit">Cadastrar</button>
-    </formulario>
+    </form>
   </main>
 </template>
 
 <script>
+import * as yup from 'yup'
+
 export default {
   data() {
     return {
@@ -99,13 +130,55 @@ export default {
       area: '',
       nivel: '',
       skills: [],
-      presentation: ''
+      presentation: '',
+
+      errors: {}
     }
   },
-  watch: {
-    area(newValue, oldValue) {
-      if (newValue !== oldValue) {
-        this.skills = []
+  methods: {
+    handleSubmit() {
+      try {
+        const schema = yup.object().shape({
+          nome: yup.string().required('Nome é obrigatório!'),
+          email: yup.string().email('Email inválido').required('Email é obrigatório!'),
+          birthDate: yup.string().required('Data de nascimento é obrigatória!'),
+          phone: yup.string().required('Whatsapp é obrigatório!'),
+          area: yup.string().required('Área de interesse é obrigatória!'),
+          nivel: yup.string().required('Nível profissional é obrigatório!'),
+          skills: yup.array().min(1, 'Selecione pelo menos uma habilidade!'),
+          presentation: yup.string().required('Carta de apresentação é obrigatória!')
+        })
+
+        schema.validateSync(
+          {
+            nome: this.name,
+            email: this.email,
+            birthDate: this.birthDate,
+            phone: this.phone,
+            area: this.area,
+            nivel: this.nivel,
+            skills: this.skills,
+            presentation: this.presentation
+          },
+          { abortEarly: false }
+        )
+      } catch (error) {
+        const captureErrorYup = (error) =>
+          error.inner.reduce((acc, currentValue) => {
+            const data = { ...acc }
+            data[currentValue.path] = currentValue.message
+            return data
+          }, {})
+
+        this.errors = captureErrorYup(error)
+      }
+    },
+
+    watch: {
+      area(newValue, oldValue) {
+        if (newValue !== oldValue) {
+          this.skills = []
+        }
       }
     }
   }
@@ -122,7 +195,7 @@ export default {
   margin-bottom: 2rem;
 }
 
-.formLogin {
+.formNew {
   display: flex;
   flex-direction: column;
   width: 100%;
@@ -177,7 +250,6 @@ textarea {
   display: flex;
   width: 100%;
   justify-content: space-between;
-  margin-bottom: 1rem;
 }
 .inputBox label {
   display: flex;
@@ -213,5 +285,14 @@ button {
 
 button:hover {
   background-color: #e8c860;
+}
+
+.inputError {
+  border-color: red;
+}
+
+.textError {
+  color: red;
+  font-size: 12px;
 }
 </style>
