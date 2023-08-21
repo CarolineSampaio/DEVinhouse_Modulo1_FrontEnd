b25c4d834cb94f21345052b3a54eab062b4a740c
Caroline Sampaio <caroline_08022@hotmail.com>
2023-08-20 22:04:55 -0300

    refactor: atualiza funcionamento menu

--- Semanas_vue/semana_10/exercicio_semana10/banco_talentos/src/App.vue
+++ Semanas_vue/semana_10/exercicio_semana10/banco_talentos/src/App.vue
@@ -1,50 +1,16 @@
 <template>
-  <header>
-    <ul>
-      <li>
-        <router-link to="/">Home</router-link>
-        <img width="35" src="./assets/icons/homeIcon.svg" alt="icone home" />
-      </li>
-
-      <li>
-        <router-link to="/talento/novo">Talentos</router-link>
-        <img width="35" src="./assets/icons/talentIcon.svg" alt="opened-folder" />
-      </li>
-      <li>
-        <router-link to="/sobre">Sobre</router-link>
-        <img width="35" src="./assets/icons/sobreIcon.svg" alt="map-marker--v1" />
-      </li>
-    </ul>
-  </header>
+  <MenuWeb />
   <main>
     <router-view></router-view>
   </main>
 </template>
 
-<script></script>
+<script>
+import MenuWeb from './components/Menu/MenuWeb.vue'
 
-<style scoped>
-ul {
-  width: 100%;
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  list-style: none;
-  margin: 30px 0px 60px;
+export default {
+  components: {
+    MenuWeb
+  }
 }
-
-li {
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  gap: 40px;
-
-  border: 1px solid #d5dee5;
-  padding: 5px 20px;
-}
-
-a {
-  text-decoration: none;
-  color: #000;
-}
-</style>
+</script>
