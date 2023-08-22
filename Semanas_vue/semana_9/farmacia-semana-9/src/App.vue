a5d7235994719a1809b95189294439d708e8c7a7
Caroline Sampaio <caroline_08022@hotmail.com>
2023-08-22 18:28:00 -0300

    feat: add dataObject para a lista de medicamentos

--- Semanas_vue/semana_9/farmacia-semana-9/src/App.vue
+++ Semanas_vue/semana_9/farmacia-semana-9/src/App.vue
@@ -10,8 +10,14 @@
 <script>
 import Header from "./components/Header.vue";
 import FormularioNovoMedicamento from "./components/FormularioNovoMedicamento.vue";
+
 export default {
   name: "App",
+  data() {
+    return {
+      listaMedicamentos: [],
+    };
+  },
   components: {
     Header,
     FormularioNovoMedicamento,
