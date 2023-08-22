05cdf9dcb9bd09d47d685eec82097732b873ca82
Caroline Sampaio <caroline_08022@hotmail.com>
2023-08-22 18:24:36 -0300

    feat: add componente Form cadastro de medicamento

--- Semanas_vue/semana_9/farmacia-semana-9/src/App.vue
+++ Semanas_vue/semana_9/farmacia-semana-9/src/App.vue
@@ -2,16 +2,19 @@
   <div></div>
   <div>
     <Header />
+    <FormularioNovoMedicamento />
   </div>
 </template>
 
 <script></script>
 <script>
 import Header from "./components/Header.vue";
+import FormularioNovoMedicamento from "./components/FormularioNovoMedicamento.vue";
 export default {
   name: "App",
   components: {
     Header,
+    FormularioNovoMedicamento,
   },
 };
 </script>
---
+++ Semanas_vue/semana_9/farmacia-semana-9/src/components/FormularioNovoMedicamento.vue
@@ -0,0 +1,113 @@
+<template>
+  <main>
+    <h3>Formulário de cadastro - novo medicamento:</h3>
+    <form class="formCadastro">
+      <div class="conteinerInput">
+        <label for="nome">Nome do medicamento</label>
+        <input
+          type="text"
+          id="nome"
+          placeholder="Informe o nome do medicamento"
+        />
+      </div>
+      <div class="conteinerInput">
+        <label for="laboratorio">Nome do laboratório</label>
+        <input
+          type="text"
+          id="laboratorio"
+          placeholder="Informe o nome do laboratório"
+        />
+      </div>
+      <div class="conteinerInput">
+        <label for="preco">Preço</label>
+        <input
+          type="number"
+          step="0.01"
+          id="preco"
+          placeholder="Informe o preço"
+        />
+      </div>
+      <button type="submit">Cadastrar</button>
+    </form>
+  </main>
+</template>
+
+<script>
+export default {
+  name: "FormularioNovoMedicamento",
+};
+</script>
+
+<style scoped>
+body {
+  display: flex;
+  width: 50%;
+  height: 100vh;
+  align-items: center;
+  justify-content: center;
+}
+
+main {
+  width: 100%;
+
+  background-color: #fff;
+  padding: 20px;
+  border-radius: 10px;
+  box-shadow: 0px 0px 15px rgba(27, 111, 170, 0.2);
+}
+
+h3 {
+  text-align: center;
+  margin-bottom: 20px;
+  color: #155786;
+}
+
+.formCadastro {
+  width: 80%;
+  display: flex;
+  margin: 0 auto;
+  justify-content: space-evenly;
+  align-items: center;
+}
+
+.conteinerInput {
+  width: 30%;
+  display: flex;
+  flex-direction: column;
+  margin-bottom: 20px;
+}
+
+label {
+  font-size: 0.9rem;
+  font-weight: 600;
+  margin-bottom: 5px;
+  color: #155786;
+}
+
+input {
+  width: 80%;
+  height: 35px;
+  padding: 5px;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+  background-color: #f3f3f3;
+  outline: none;
+}
+
+button {
+  width: 10%;
+  height: 35px;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+  background-color: #3e88bd;
+
+  color: white;
+  font-size: 16px;
+  font-weight: 700;
+}
+
+button:hover {
+  background-color: #1b6faa;
+  cursor: pointer;
+}
+</style>
