40907ce35d636a48055d68dccd92bec924667250
Caroline Sampaio <caroline_08022@hotmail.com>
2023-08-21 22:59:11 -0300

    feat: add projeto pratico via cep com axios

---
+++ Semanas_vue/semana_11/via-cep-axios/src/App.vue
@@ -0,0 +1,60 @@
+<template>
+  <div>
+    <h1>Consulta de CEP:</h1>
+    <form>
+      <h3>Resultado da sua consulta:</h3>
+      <p>Cep: {{ cep.cep }}</p>
+      <p>Logradoro: {{ cep.logradouro }}</p>
+      <p>Bairro: {{ cep.bairro }}</p>
+      <p>Cidade: {{ cep.localidade }}</p>
+      <p>Estado: {{ cep.uf }}</p>
+    </form>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  data() {
+    return {
+      cep: {},
+    };
+  },
+
+  methods: {
+    consultarCep() {
+      axios
+        .get("https://viacep.com.br/ws/02046050/json/")
+        .then((response) => {
+          this.cep = response.data;
+          console.log(response.data);
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    },
+  },
+
+  mounted() {
+    this.consultarCep();
+  },
+};
+</script>
+<style scoped>
+h1 {
+  font-size: 1.5rem;
+  margin: 20px 0px;
+  text-align: center;
+}
+h3 {
+  font-size: 1.2rem;
+  font-weight: 500;
+}
+
+p {
+  font-size: 1rem;
+  font-weight: 400;
+  margin: 10px 0px;
+}
+</style>
