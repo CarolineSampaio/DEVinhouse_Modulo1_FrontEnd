49559207ba4cc2a4ec8878c884de6036600b5071
Caroline Sampaio <caroline_08022@hotmail.com>
2023-08-22 14:11:08 -0300

    feat: add ex cadastro com axios e json-server

--- Semanas_vue/semana_11/ciclos-de-vida-axios/src/App.vue
+++ Semanas_vue/semana_11/ciclos-de-vida-axios/src/App.vue
@@ -1,21 +1,62 @@
 <template>
-  <h1>Página inicial</h1>
+  <h1>Formulário de cadastro de novo cliente</h1>
+  <form @submit.prevent="handleSubmit">
+    <input type="text" placeholder="Digite seu nome" v-model="cliente.nome" />
+    <input
+      type="email"
+      placeholder="Digite seu email"
+      v-model="cliente.email"
+    />
+    <button type="submit">Cadastrar cliente</button>
+  </form>
+  <hr />
+  <br />
+  <h2>Lista de clientes</h2>
+  {{ listaClientes }}
 </template>
 
 <script>
 import axios from "axios";
 
 export default {
+  data() {
+    return {
+      cliente: {
+        nome: "",
+        email: "",
+      },
+
+      listaClientes: [],
+    };
+  },
+  methods: {
+    handleSubmit() {
+      axios
+        .post("http://localhost:50001/clientes", this.cliente)
+        .then((res) => {
+          alert("Cliente cadastrado com sucesso!");
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+
+      this.getDados();
+    },
+
+    getDados() {
+      axios
+        .get("http://localhost:50001/clientes")
+        .then((response) => {
+          this.listaClientes = response.data;
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    },
+  },
+
   mounted() {
-    axios
-      .get("https://api.github.com/users/CarolineSampaio")
-      .then((response) => {
-        console.log(response.data);
-        console.log(response.data.name);
-      })
-      .catch((error) => {
-        console.log(error);
-      });
+    this.getDados();
   },
 };
 </script>
---
+++ Semanas_vue/semana_11/ciclos-de-vida-axios/src/components/ExemploGithub.vue
@@ -0,0 +1,26 @@
+<template>
+  <h1>Página inicial</h1>
+  <h2>Nome: {{ usuario.name }}</h2>
+  <img :src="usuario.avatar_url" :width="200" />
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  data() {
+    return {
+      usuario: {},
+    };
+  },
+
+  mounted() {
+    axios
+      .get("https://api.github.com/users/CarolineSampaio")
+      .then((res) => (this.usuario = res.data))
+      .catch((erro) => console.log(erro));
+  },
+};
+</script>
+
+<style scoped></style>
