9316f9077cd76b69bc2dec256c92decf074f8f64
Caroline Sampaio <caroline_08022@hotmail.com>
2023-08-21 22:02:04 -0300

    feat: add ex projeto básico com axios

---
+++ Semanas_vue/semana_11/ciclos-de-vida-axios/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <h1>Página inicial</h1>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  mounted() {
+    axios
+      .get("https://api.github.com/users/CarolineSampaio")
+      .then((response) => {
+        console.log(response.data);
+        console.log(response.data.name);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  },
+};
+</script>
+
+<style scoped></style>
