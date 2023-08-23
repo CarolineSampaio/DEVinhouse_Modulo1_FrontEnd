503aeca08fcf22130d89fe8ee85e09ac16409e01
Caroline Sampaio <caroline_08022@hotmail.com>
2023-08-23 15:59:59 -0300

    feat: add formulario basico com validação vuetify

--- Semanas_vue/semana_11/vuetify-do-zero/src/views/AboutView.vue
+++ Semanas_vue/semana_11/vuetify-do-zero/src/views/AboutView.vue
@@ -1,7 +1,71 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+  <v-form ref="form" @submit.prevent="handleSubmit">
+    <v-text-field
+      label="Nome"
+      variant="outlined"
+      :rules="[(v) => !!v || 'Nome é obrigatório']"
+      v-model="nome"
+    ></v-text-field>
+
+    <v-text-field
+      label="Email"
+      variant="outlined"
+      :rules="[(v) => !!v || 'Email é obrigatório']"
+      v-model="email"
+    ></v-text-field>
+
+    <v-select
+      label="Estado"
+      variant="outlined"
+      :items="estadosList"
+      :rules="[(v) => !!v || 'Estado é obrigatório']"
+      v-model="estado"
+    >
+    </v-select>
+
+    <v-btn variant="tonal" color="green" type="submit">Cadastrar</v-btn>
+    <v-btn variant="tonal" color="orange" type="reset">Limpar</v-btn>
+  </v-form>
+
+  <v-snackbar v-model="snackbar" :timeout="timeout">
+    {{ text }}
+
+    <template v-slot:actions>
+      <v-btn color="red" variant="text" @click="snackbar = false">
+        Fechar
+      </v-btn>
+    </template>
+  </v-snackbar>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      snackbar: false,
+      timeout: 2000,
+      text: "",
+      nome: "",
+      email: "",
+      estado: "",
+      estadosList: ["CE", "SP", "SC", "PR", "RS", "PE"],
+    };
+  },
+  methods: {
+    async handleSubmit() {
+      const { valid } = await this.$refs.form.validate();
+
+      if (valid) {
+        this.text = "Usuário cadastrado com sucesso!";
+        this.snackbar = true;
+        this.$refs.form.reset();
+      } else {
+        this.text = "Preencha todos os campos corretamente!";
+        this.snackbar = true;
+      }
+    },
+  },
+};
+</script>
 
 <style></style>
