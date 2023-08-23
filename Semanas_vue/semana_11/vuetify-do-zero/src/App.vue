148373ee65b4167da5c860e0f5cabadacf9d6cd6
Caroline Sampaio <caroline_08022@hotmail.com>
2023-08-23 14:17:53 -0300

    feat: add ex botão e card vuetify

--- Semanas_vue/semana_11/vuetify-do-zero/src/App.vue
+++ Semanas_vue/semana_11/vuetify-do-zero/src/App.vue
@@ -15,4 +15,8 @@ import { RouterLink, RouterView } from "vue-router";
   <RouterView />
 </template>
 
-<style scoped></style>
+<style scoped>
+header {
+  margin-left: 50px;
+}
+</style>
--- Semanas_vue/semana_11/vuetify-do-zero/src/views/HomeView.vue
+++ Semanas_vue/semana_11/vuetify-do-zero/src/views/HomeView.vue
@@ -3,5 +3,35 @@
 <template>
   <main>
     <h1>Home</h1>
+    <v-btn> Button </v-btn>
+    <v-btn variant="outlined"> Button </v-btn>
+    <v-btn variant="tonal"> Button </v-btn>
+    <v-btn variant="text"> Button </v-btn>
+    <br />
+
+    <v-btn color="blue" append-icon="mdi-check-bold"> Button </v-btn>
+    <v-btn prepend-icon="mdi-check-bold"> Button </v-btn>
+    <v-btn color="green" prepend-icon="mdi-check-bold"> Button </v-btn>
+    <v-btn icon="mdi-check-bold"></v-btn>
+
+    <v-card
+      title="Título do meu Card"
+      subtitle="Subtítulo do meu Card"
+      text="Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam
+        voluptatum, quibusdam, quia, quos voluptates voluptatem quod
+        exercitationem voluptas quas doloribus quidem. Quisquam voluptatum,
+        quibusdam, quia, quos voluptates voluptatem quod exercitationem voluptas
+        quas doloribus quidem."
+      variant="tonal"
+    ></v-card>
   </main>
 </template>
+
+<style>
+main {
+  margin-left: 50px;
+}
+button {
+  margin: 10px;
+}
+</style>
