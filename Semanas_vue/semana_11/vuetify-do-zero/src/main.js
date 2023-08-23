7a5cf4cd9909755d1e4959e07d7814029651e305
Caroline Sampaio <caroline_08022@hotmail.com>
2023-08-23 12:53:44 -0300

    feat: add projeto inicial configurado vuetify

---
+++ Semanas_vue/semana_11/vuetify-do-zero/src/main.js
@@ -0,0 +1,19 @@
+import "./assets/main.css";
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
+
+// Vuetify
+import "vuetify/styles";
+import "@mdi/font/css/materialdesignicons.css";
+import { createVuetify } from "vuetify";
+import * as components from "vuetify/components";
+import * as directives from "vuetify/directives";
+
+const vuetify = createVuetify({
+  components,
+  directives,
+  icons: { defaultSet: "mdi" },
+});
+
+createApp(App).use(router).use(vuetify).mount("#app");
