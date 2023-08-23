ae2b160e924c352edfcbcc80a0a3e2b3dd64bf46
Caroline Sampaio <caroline_08022@hotmail.com>
2023-08-23 17:05:35 -0300

    feat: add pag home dinâmica com vuetify

--- Semanas_vue/semana_11/clone_instagram/src/main.js
+++ Semanas_vue/semana_11/clone_instagram/src/main.js
@@ -3,6 +3,20 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 
+import 'vuetify/styles'
+import '@mdi/font/css/materialdesignicons.css'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+
+const vuetify = createVuetify({
+  components,
+  directives,
+  icons: {
+    defaultSet: 'mdi'
+  }
+})
+
 import router from './routes/routes.js'
 
-createApp(App).use(router).mount('#app')
+createApp(App).use(vuetify).use(router).mount('#app')
