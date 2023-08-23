0b50f7f662ed55e08d7ea1383760814021b0d8da
Caroline Sampaio <caroline_08022@hotmail.com>
2023-08-23 12:18:56 -0300

    feat: add exemplo criação vuetify comando create

---
+++ Semanas_vue/semana_11/vuetify-create/src/layouts/default/Default.vue
@@ -0,0 +1,13 @@
+<!-- eslint-disable vue/multi-word-component-names -->
+<template>
+  <v-app>
+    <default-bar />
+
+    <default-view />
+  </v-app>
+</template>
+
+<script setup>
+import DefaultBar from "./AppBar.vue";
+import DefaultView from "./View.vue";
+</script>
