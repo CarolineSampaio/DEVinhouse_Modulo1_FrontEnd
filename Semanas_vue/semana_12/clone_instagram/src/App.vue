fd27be2321f4f7252863a6498916b2d393af1a1a
Caroline Sampaio <caroline_08022@hotmail.com>
2023-08-27 19:01:08 -0300

    feat: add menu dinamico, com v-if e path da pagina

--- Semanas_vue/semana_12/clone_instagram/src/App.vue
+++ Semanas_vue/semana_12/clone_instagram/src/App.vue
@@ -1,3 +1,33 @@
 <template>
-  <router-view />
+  <v-layout>
+    <v-app-bar color="blue" v-if="renderizarMenu">
+      <v-app-bar-title class="titleMenu">Instagram</v-app-bar-title>
+
+      <v-btn icon>
+        <router-link to="/posts/novo">
+          <v-icon color="#FFF">mdi-note</v-icon>
+        </router-link>
+      </v-btn>
+
+      <v-btn>
+        <router-link to="/posts">
+          <v-icon color="#FFF">mdi-note-multiple</v-icon>
+        </router-link>
+      </v-btn>
+    </v-app-bar>
+
+    <v-main>
+      <router-view></router-view>
+    </v-main>
+  </v-layout>
 </template>
+
+<script>
+export default {
+  computed: {
+    renderizarMenu() {
+      return this.$route.path !== '/' && this.$route.path !== '/cadastro'
+    }
+  }
+}
+</script>
