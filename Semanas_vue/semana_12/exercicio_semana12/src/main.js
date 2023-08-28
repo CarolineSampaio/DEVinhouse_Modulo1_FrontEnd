2347c9e95d4d7083a0a4f5e458eaa90366678df0
Caroline Sampaio <caroline_08022@hotmail.com>
2023-08-28 18:02:04 -0300

    feat: add config json-server e vuetify

--- Semanas_vue/semana_12/exercicio_semana12/src/main.js
+++ Semanas_vue/semana_12/exercicio_semana12/src/main.js
@@ -4,4 +4,18 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './routes/routes.js'
 
-createApp(App).use(router).mount('#app')
+import 'vuetify/styles'
+import '@mdi/font/css/materialdesignicons.css'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+
+const vuetify = createVuetify({
+  components,
+  directives,
+  icons: {
+    defaultSet: 'mdi'
+  }
+})
+
+createApp(App).use(router).use(vuetify).mount('#app')
