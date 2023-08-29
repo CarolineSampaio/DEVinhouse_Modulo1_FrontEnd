5f8043ff392f6486cf3867f9051c3fde462ad800
Caroline Sampaio <caroline_08022@hotmail.com>
2023-08-29 14:45:29 -0300

    feat: add página carrinho estrutura inicial

--- Semanas_vue/semana_12/exercicio_semana12/src/views/Cart/CartPag.vue
+++ Semanas_vue/semana_12/exercicio_semana12/src/views/Cart/CartPag.vue
@@ -1,3 +1,34 @@
 <template>
-  <div>Página Cart</div>
+  <v-table>
+    <thead>
+      <tr>
+        <th class="text-left">#</th>
+        <th class="text-left">Produto</th>
+        <th class="text-left">Quantidade</th>
+        <th class="text-left">Preço</th>
+        <th class="text-left">Subtotal</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr v-for="item in this.$store.state.produtosCarrinho" :key="item.id">
+        <td><img :src="item.imagem" width="30" /></td>
+        <td>{{ item.nome }}</td>
+        <td>{{ item.quantidade }}</td>
+        <td>
+          {{
+            new Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'BRL' }).format(
+              item.preco
+            )
+          }}
+        </td>
+        <td>
+          {{
+            new Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'BRL' }).format(
+              item.quantidade * item.preco
+            )
+          }}
+        </td>
+      </tr>
+    </tbody>
+  </v-table>
 </template>
